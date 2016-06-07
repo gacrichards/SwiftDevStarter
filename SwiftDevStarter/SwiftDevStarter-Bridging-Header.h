fb4b9252027b4c477982fb7249c83af7ae92e14d
Cole <g.a.cole.richards@gmail.com>
2016-06-07 16:09:37 -0600

    Adding Bridging Header for ObjC Swifter interoperability

---
+++ SwiftDevStarter/SwiftDevStarter-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftDevStarter-Bridging-Header.h
+//  SwiftDevStarter
+//
+//  Created by Cole Richards on 6/7/16.
+//  Copyright © 2016 ROXIMITY. All rights reserved.
+//
+
+#ifndef SwiftDevStarter_Bridging_Header_h
+#define SwiftDevStarter_Bridging_Header_h
+
+#import "ROXIMITYSDK/ROXIMITYSDK.h"
+
+#endif /* SwiftDevStarter_Bridging_Header_h */
