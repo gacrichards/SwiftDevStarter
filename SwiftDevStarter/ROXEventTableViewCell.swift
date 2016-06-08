8976bf32a4f1510ca0f953a33dff11f2bcd9681b
Cole <g.a.cole.richards@gmail.com>
2016-06-08 10:44:19 -0600

    Creating event tableviewcell xib

---
+++ SwiftDevStarter/ROXEventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ROXEventTableViewCell.swift
+//  SwiftDevStarter
+//
+//  Created by Cole Richards on 6/8/16.
+//  Copyright © 2016 ROXIMITY. All rights reserved.
+//
+
+import UIKit
+
+class ROXEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
